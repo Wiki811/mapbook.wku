d6e75d3bf0f6a0309c200e3a2ffa27744dfb3b96
Wiki811 <wiktoria.kucharczyk@student.wat.edu.pl>
2026-04-16 14:25:07 +0200

    wip:dynamic menu

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+while True:
+    print('0 - zakończ program')
+    print('1 - wyświetl znajomych')
 
-from mapbook_wku.model import users
-from mapbook_wku.controller import read_data
-
+    choose=input('wybierz opcję: ')
+    if choose =='0':
+        break
+    if choose == '1':
 read_data(users[1:])
