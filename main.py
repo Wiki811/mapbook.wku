566a8a545a61b549130928752f40750060f6467a
Wiki811 <wiktoria.kucharczyk@student.wat.edu.pl>
2026-04-16 14:00:29 +0200

    wip: mvc template applied

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-users: list = [
-    {'username': 'oliwia', 'location': 'łódź', 'posts': 1,
-     'usermessage': ['życzenia1', 'kocham legie1', 'sprzedam opla', 'kiwi']},
-    {'username': 'paweł', 'location': 'ostróda', 'posts': 2,
-     'usermessage': ['życzenia2' 'kocham legie2', 'sprzedam opla', 'kiwi']},
-    {'username': 'eliza', 'location': 'radom', 'posts': 3, 'usermessage': ['życzenia3' 'kocham legie3']},
-    {'username': 'filip', 'location': 'dęblin', 'posts': 4,
-     'usermessage': ['życzenia3' 'kocham legie4', 'sprzedam opla', 'kiwi']},
-]
 
-
-def read_data(users_data: list) -> None:
-    for user in users_data:
-        print(
-            f'twój znajomy {user['username']} z miejscowości {user["location"]} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}.')
+from mapbook_wku.model import users
+from mapbook_wku.controller import read_data
 
 read_data(users[1:])
---
+++ mapbook_wku/model.py
@@ -0,0 +1,9 @@
+users: list = [
+    {'username': 'oliwia', 'location': 'łódź', 'posts': 1,
+     'usermessage': ['życzenia1', 'kocham legie1', 'sprzedam opla', 'kiwi']},
+    {'username': 'paweł', 'location': 'ostróda', 'posts': 2,
+     'usermessage': ['życzenia2' 'kocham legie2', 'sprzedam opla', 'kiwi']},
+    {'username': 'eliza', 'location': 'radom', 'posts': 3, 'usermessage': ['życzenia3' 'kocham legie3']},
+    {'username': 'filip', 'location': 'dęblin', 'posts': 4,
+     'usermessage': ['życzenia3' 'kocham legie4', 'sprzedam opla', 'kiwi']},
+]
