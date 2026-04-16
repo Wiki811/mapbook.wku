73cf55d6d55a958d3204b3a249973821d8a0a85b
Wiki811 <wiktoria.kucharczyk@student.wat.edu.pl>
2026-04-16 12:42:03 +0200

    wip: simple structure of users list

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('geodezja i geoinformatyka rządzi1111')
+zmienna_1: str = "oliwia"
+zmienna_2: str = "paweł"
+zmienna_3: str = "eliza"
+zmienna_4: str = "filip"
+zmienna_5: str = "weronika"
+zmienna_6: str = "ewela"
+print(zmienna_1, zmienna_2)
+print(zmienna_3)
+print(zmienna_4)
+print(zmienna_5)
+print(zmienna_6)
