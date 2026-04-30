e0efb1c7604b76f38bd78be7fb7d39f53a6c238a
Wiki811 <wiktoria.kucharczyk@student.wat.edu.pl>
2026-04-30 15:02:06 +0200

    feat: add location function

--- notatnik.py
+++ notatnik.py
@@ -1,35 +1,39 @@
-# moja_lista_na_sok=['banan','marchew','młotek']
-# print(moja_lista_na_sok)
-#
-# moja_lista_na_sok.pop(2)
-# print(moja_lista_na_sok)
-#
-#
-# moja_lista_na_sok.remove('banan')
-# print(moja_lista_na_sok)
-
 
 users: list = [
-    {'username': 'oliwia', 'location': 'łódź', 'posts': 1,
+    {'username': 'oliwia', 'location': 'Łódź', 'posts': 1,
+     'usermessage': ['życzenia1', 'kocham legie1', 'sprzedam opla', 'kiwi']}, {'username': 'ola', 'location': 'Kraków', 'posts': 1,
      'usermessage': ['życzenia1', 'kocham legie1', 'sprzedam opla', 'kiwi']},
-    {'username': 'paweł', 'location': 'ostróda', 'posts': 2,
-     'usermessage': ['życzenia2' 'kocham legie2', 'sprzedam opla', 'kiwi']},
-    {'username': 'eliza', 'location': 'radom', 'posts': 3, 'usermessage': ['życzenia3' 'kocham legie3']},
-    {'username': 'filip', 'location': 'dęblin', 'posts': 4,
-     'usermessage': ['życzenia3' 'kocham legie4', 'sprzedam opla', 'kiwi']},
 ]
-# print(users)
-# users.remove({'username': 'oliwia', 'location': 'łódź', 'posts': 1,
-#      'usermessage': ['życzenia1', 'kocham legie1', 'sprzedam opla', 'kiwi']})
 
-def update_user(users_data: list)->None:
-    name=input('podaj imię użytkownika do zmiany: ')
+import folium
+import requests
+from bs4 import BeautifulSoup
 
+def get_coordinates(location:str)->list:
+
+    url=f'https://pl.wikipedia.org/wiki/{location}'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+    response = requests.get(url, headers=headers)
+
+    response_html=BeautifulSoup(response.text,'html.parser')
+    response_html_latitude=float(response_html.select('.latitude')[1].text.replace(',','.'))
+    response_html_longitude=float(response_html.select('.longitude')[1].text.replace(',','.'))
+    return [response_html_latitude,response_html_longitude]
+
+
+# for user in users:
+#     print(get_coordinates(location=user['location']))
+
+def get_mapa(users_data: list)->None:
+
+    m = folium.Map([52, 21], zoom_start=12)
     for user in users_data:
-        if user['username'] == name:
-            user['username']=input('podaj nowe imię: ')
-            user['location']=input('podaj nową lokalizację: ')
-            user['posts']=int(input('podaj liczbę postów: '))
-update_user(users)
-print(users)
+        folium.Marker(
+            location=get_coordinates(user['location']),
+            tooltip="Click me!",
+            popup="Mt. Hood Meadows",
+            icon=folium.Icon(icon="cloud"),
+        ).add_to(m)
 
+    m.save('mapa.html')
